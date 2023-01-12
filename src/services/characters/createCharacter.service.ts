3d5c22e805b541d1bfae15ce3a12a3a2d014172a
Guilherme Wallace <gpeixoto938@gmail.com>
2023-01-12 16:50:41 -0300

    feat/characters: created routers characters

--- src/services/characters/createCharacter.service.ts
+++ src/services/characters/createCharacter.service.ts
@@ -1,15 +1,25 @@
+import { User } from "../../entities/user.entity";
 import AppDataSource from "./../../data-source";
 import Character from "./../../entities/character.entity";
+import { ICharacterRequest } from "./../../interfaces/characters/index";
 
-const createCharacterService = async (userData): Promise<{}> => {
+const createCharacterService = async (
+  characterData: ICharacterRequest,
+  userId: string
+): Promise<Character> => {
   //CÓDIGO AQUI
 
   const characterRepo = AppDataSource.getRepository(Character);
+  const userRepo = AppDataSource.getRepository(User);
+  const user = await userRepo.findOneBy({ id: userId });
 
-  const campaign = characterRepo.create(userData);
+  const createdCharacter = characterRepo.create({
+    ...characterData,
+    user: user,
+  });
+  await characterRepo.save(createdCharacter);
 
-  await characterRepo.save(campaign);
-  return campaign;
+  return createdCharacter;
 };
 
 export default createCharacterService;
--- src/services/characters/deleteCharacter.service.ts
+++ src/services/characters/deleteCharacter.service.ts
@@ -1,11 +1,23 @@
 import AppDataSource from "../../data-source";
+import { User } from "../../entities/user.entity";
 import { AppError } from "../../errors/AppError";
 import Character from "./../../entities/character.entity";
 
-const deleteCharacterService = async (id: string) => {
+const deleteCharacterService = async (characterId: string, userId: string) => {
   const characterRepository = AppDataSource.getRepository(Character);
-  const character = await characterRepository.findOneBy({ id: id });
+  const character = await characterRepository.findOneBy({ id: characterId });
 
+  const findCharacter = await characterRepository.findOne({
+    where: {
+      id: characterId,
+    },
+    relations: {
+      user: true,
+    },
+  });
+  if (findCharacter.user.id !== userId) {
+    throw new AppError("character must be yours", 409);
+  }
   if (!character.isActive) {
     throw new AppError("user must be active", 400);
   }
