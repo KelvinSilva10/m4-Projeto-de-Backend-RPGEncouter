121e6185481aca8d112701a092466ad71652bbe0
Gabriel Larangeira <106683361+GabrielLarangeira@users.noreply.github.com>
2023-01-17 10:17:37 -0300

    feat/testes: create tests integration and mocks repository

--- src/__tests__/integration/User/create.route.test.ts
+++ src/__tests__/integration/User/create.route.test.ts
@@ -4,3 +4,30 @@ import app from "../../../app";
 import { User } from "../../../entities/user.entity";
 import AppDataSource from "../../../data-source";
 
+describe("Create user route tests", () => {
+    let conn: DataSource
+    const baseUrl: string = "/users"
+    const userRepo: Repository<User> = AppDataSource.getRepository(User)
+
+    beforeAll(async () => {
+
+        await AppDataSource.initialize()
+            .then((res) => (conn = res))
+            .catch((err) => console.error(err))
+    })
+
+    beforeEach(async () => {
+
+        const users = await userRepo.find()
+        await userRepo.remove(users)
+    })
+
+    afterAll(async () => {
+
+        await conn.destroy
+    })
+
+    it("Should be able to create user", async () => {
+        
+    })
+})
